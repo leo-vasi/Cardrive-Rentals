95bb11706ac3292295a1268197ca4635d8f22940
Leonardo Silva <levasi.leonardo55@gmail.com>
2025-03-17 16:32:59 -0300

    getAddressById, createAddress, updateAddress and deleteAddress

--- cardriverentals/src/main/java/com/leo/cardriverentals/controller/AddressController.java
+++ cardriverentals/src/main/java/com/leo/cardriverentals/controller/AddressController.java
@@ -1,13 +1,15 @@
 package com.leo.cardriverentals.controller;
 
 import com.leo.cardriverentals.model.Address;
+import com.leo.cardriverentals.model.Rental;
 import com.leo.cardriverentals.service.AddressService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/addresses")
@@ -26,6 +28,44 @@ public class AddressController {
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.ok(addresses);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable Long id) {
+        Optional<Address> address = addressService.getAddresById(id);
+        if (address.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            return ResponseEntity.ok(address.get());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Address> createAddress (@Valid @RequestBody Address address) {
+        Address createAddress = addressService.createAddress(address);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createAddress);
+    }
+
+    @PutMapping("/alter/{id}")
+    public ResponseEntity<Address> updateAddress (@PathVariable Long id, @RequestBody Address address) {
+        Optional<Address> existingAddress = addressService.getAddresById(id);
+        if (existingAddress.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            Address updateAddress = addressService.updateAddress(id, address);
+            return ResponseEntity.ok(updateAddress);
+        }
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteAddress (@PathVariable Long id) {
+        if (addressService.getAddresById(id).isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            addressService.deleteAddress(id);
+            return ResponseEntity.noContent().build();
         }
     }
 }
--- cardriverentals/src/main/java/com/leo/cardriverentals/service/AddressService.java
+++ cardriverentals/src/main/java/com/leo/cardriverentals/service/AddressService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AddressService {
@@ -19,6 +20,31 @@ public class AddressService {
 
     public List<Address> getAddresses() {
         return addressRepository.findAll();
+    }
+
+    public Optional<Address> getAddresById (Long id) {
+        return addressRepository.findById(id);
+    }
+
+    public Address createAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+    public Address updateAddress (Long id, Address address) {
+        Address existingAddress = addressRepository.findById(id).orElseThrow(()-> new RuntimeException("Address Not Found"));
+        existingAddress.setUser(address.getUser());
+        existingAddress.setNum(address.getNum());
+        existingAddress.setStreet(address.getStreet());
+        existingAddress.setCity(address.getCity());
+        existingAddress.setState(address.getState());
+        existingAddress.setCountry(address.getCountry());
+        existingAddress.setZipcode(address.getZipcode());
+        existingAddress.setAddressType(address.getAddressType());
+        return addressRepository.save(existingAddress);
+    }
+
+    public void deleteAddress (Long id) {
+        addressRepository.deleteById(id);
     }
 
 }
