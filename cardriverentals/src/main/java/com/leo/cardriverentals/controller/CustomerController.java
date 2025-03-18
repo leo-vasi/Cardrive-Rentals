81618d786f81959da5ba44635737099dcbc2e9ca
Leonardo Silva <levasi.leonardo55@gmail.com>
2025-03-18 10:20:27 -0300

    getCustomerById, createCustomer, updateCustomer and deleteCustomer

--- cardriverentals/src/main/java/com/leo/cardriverentals/controller/CustomerController.java
+++ cardriverentals/src/main/java/com/leo/cardriverentals/controller/CustomerController.java
@@ -1,13 +1,15 @@
 package com.leo.cardriverentals.controller;
 
 import com.leo.cardriverentals.model.Customer;
+import com.leo.cardriverentals.model.Employee;
 import com.leo.cardriverentals.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customers")
@@ -26,6 +28,43 @@ public class CustomerController {
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.ok(customers);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById (@PathVariable Long id) {
+        Optional<Customer> customer = customerService.getCustomerById(id);
+        if (customer.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            return ResponseEntity.ok(customer.get());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer (@Valid @RequestBody Customer customer) {
+        Customer createdCustomer = customerService.createCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
+    }
+
+    @PutMapping("/alter/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+        Optional<Customer> existingCustomer = customerService.getCustomerById(id);
+        if (existingCustomer.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            Customer updatedCustomer = customerService.updateCustomer(id, customer);
+            return ResponseEntity.ok(updatedCustomer);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        if (customerService.getCustomerById(id).isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            customerService.deleteCustomer(id);
+            return ResponseEntity.noContent().build();
         }
     }
 }
--- cardriverentals/src/main/java/com/leo/cardriverentals/service/CustomerService.java
+++ cardriverentals/src/main/java/com/leo/cardriverentals/service/CustomerService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -19,5 +20,31 @@ public class CustomerService {
 
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById (Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer (Long id, Customer customer) {
+        Customer existingCustomer = customerRepository.findById(id).orElseThrow(()-> new RuntimeException("No Customer Found"));
+        existingCustomer.setName(customer.getName());
+        existingCustomer.setGender(customer.getGender());
+        existingCustomer.setBirthDate(customer.getBirthDate());
+        existingCustomer.setCpf(customer.getCpf());
+        existingCustomer.setPhone(customer.getPhone());
+        existingCustomer.setEmail(customer.getEmail());
+        existingCustomer.setPassword(customer.getPassword());
+        existingCustomer.setRentalHistory(customer.getRentalHistory());
+        existingCustomer.setLoginHistory(customer.getLoginHistory());
+        return customerRepository.save(existingCustomer);
+    }
+
+    public void deleteCustomer (Long id) {
+        customerRepository.deleteById(id);
     }
 }
