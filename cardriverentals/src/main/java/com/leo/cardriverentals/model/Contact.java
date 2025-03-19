c9586306b3973a9e1dc256e0717faee23f577fb6
Leonardo Silva <levasi.leonardo55@gmail.com>
2025-03-19 14:29:17 -0300

    Refactoring entities, model class Contact added

---
+++ cardriverentals/src/main/java/com/leo/cardriverentals/model/Contact.java
@@ -0,0 +1,45 @@
+package com.leo.cardriverentals.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
+    private Long contactId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotBlank(message = "O telefone não pode estar em branco")
+    @Pattern(regexp = "\\d{10,15}", message = "O telefone deve ter entre 10 e 15 números")
+    @Column(name = "phone", nullable = false, length = 15)
+    private String phone;
+
+    @NotBlank(message = "O e-mail não pode estar em branco")
+    @Email(message = "Formato de e-mail inválido")
+    @Size(max = 50, message = "O e-mail deve ter no máximo 50 caracteres")
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Email(message = "Formato de e-mail de recuperação inválido")
+    @Size(max = 50, message = "O e-mail de recuperação deve ter no máximo 50 caracteres")
+    @Column(name = "recovery_email", length = 50)
+    private String recoveryEmail;
+}
